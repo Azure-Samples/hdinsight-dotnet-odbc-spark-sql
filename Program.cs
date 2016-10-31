9bfc427cb559924b915a5ef4a66c0b9b00f8d9eb
Bharath Venkatesh <bhvenkat@microsoft.com>
2016-10-31 13:37:15 -0700

    Adding Disclaimer

--- Program.cs
+++ Program.cs
@@ -1,4 +1,14 @@
-﻿using System;
+/*DISCLAIMER: © 2016 Microsoft Corporation. All rights reserved. Sample scripts in this guide are not supported under any
+Microsoft standard support program or service. The sample scripts are provided AS IS without warranty of any kind. 
+Microsoft disclaims all implied warranties including, without limitation, any implied warranties of merchantability or 
+of fitness for a particular purpose. The entire risk arising out of the use or performance of the sample scripts and 
+documentation remains with you. In no event shall Microsoft, its authors, or anyone else involved in the creation, 
+production, or delivery of the scripts be liable for any damages whatsoever (including, without limitation, damages 
+for loss of business profits, business interruption, loss of business information, or other pecuniary loss) arising 
+out of the use of or inability to use the sample scripts or documentation, even if Microsoft has been advised of the 
+possibility of such damages.*/
+    
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Data.Odbc;
